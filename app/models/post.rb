6380062e557c83d851530c8db38e3c9c499fa31d
Mark Viti <+>
2022-06-28 10:51:32 -0400

    commit tree 13a08e93bb4756dbc96cf272cc1570435de4eff7

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction), message: "%{value} is not a valid category" }
+    validates :content, length: { minimum: 100 }
 end
